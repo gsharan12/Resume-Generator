140c9c14a89869daeb84b4c77e9224924677982b
Gaurav <gaurav.sharanrd@gmail.com>
2022-07-19 19:27:17 +0530

    Ready !!
    Ready to enter information and generate a Resume based on it which can be downloaded too.

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function addNewAQField()
 {
     let newNode=document.createElement("textarea");
     newNode.classList.add("form-control");
-    newNode.classList.add("aqField");
+    newNode.classList.add("eqField");
     newNode.classList.add("mt-2");
     newNode.setAttribute("rows", 3);
     newNode.setAttribute("placeholder","Enter here");
@@ -29,4 +29,55 @@ function addNewAQField()
     let aqAddButtonOb=document.getElementById("aqAddButton");
 
     aqOb.insertBefore(newNode, aqAddButtonOb);
+}
+
+function generateCV() {
+    let nameField=document.getElementById('nameField').value
+    let nameT1=document.getElementById("nameT1");
+    nameT1.innerHTML=nameField;
+
+    document.getElementById("nameT2").innerHTML=nameField;
+
+    document.getElementById("contactT").innerHTML=document.getElementById("contactField").value
+
+    document.getElementById("addressT").innerHTML=document.getElementById("addressField").value
+
+    document.getElementById("gitT").innerHTML=document.getElementById("githubfield").value
+
+    document.getElementById("linkedT").innerHTML=document.getElementById("linkedinfield").value
+
+    document.getElementById("portT").innerHTML=document.getElementById("portfoliofield").value
+
+    document.getElementById("objectiveT").innerHTML=document.getElementById("objectiveField").value
+
+    let wes=document.getElementsByClassName("weField");
+
+    let str="";
+
+    for(let e of wes)
+    {
+        str=str+`<li> ${e.value} </li>`;
+    }
+    document.getElementById("weT").innerHTML=str;
+
+
+
+
+    let aqs=document.getElementsByClassName("eqField");
+
+    let str1="";
+
+    for(let f of aqs)
+    {
+        str1+= `<li> ${f.value} </li>`;
+    }
+    document.getElementById("aqT").innerHTML=str1;
+
+    document.getElementById("cv-form").style.display="none";
+    document.getElementById("cv-template").style.display="block";
+    
+}
+
+function printCV() {
+    window.print();
 }
